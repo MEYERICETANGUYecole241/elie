aea7433db64e551d9db536c50f02503dff992736
ICE Tanguy <icetanguy065@gmail.com>
2025-05-26 02:07:37 +0100

    elie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 
 import Header from "./component/Header";
+import Heroe from "./component/Heroe";
 
 export default function Home() {
   return (
     <div>
 
-       <Header/>
+      <Header/>
+      <Heroe/>
     </div>
   );
 }
